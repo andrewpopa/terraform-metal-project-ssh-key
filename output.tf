0422b68d434512b089f608a2c16f24621e98d201
Andrei Popa <andrei.popa@eu.equinix.com>
2021-10-08 11:52:12 +0200

    migrate to metal provider

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
 output "id" {
   description = "The unique ID of the key"
-  value       = packet_project_ssh_key.this.id
+  value       = metal_project_ssh_key.this.id
 }
 
 output "name" {
   description = "The name of SSH key"
-  value       = packet_project_ssh_key.this.name
+  value       = metal_project_ssh_key.this.name
 }
 
 output "public_key" {
   description = "The public SSH key"
-  value       = packet_project_ssh_key.this.public_key
+  value       = metal_project_ssh_key.this.public_key
 }
 
 output "private_key_filename" {
