7a952d576b06c49189070d5f6f6e4c08416b8aff
Andrei Popa <andrei.popa@eu.equinix.com>
2021-10-08 11:16:17 +0200

    add code

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "id" {
+  description = "The unique ID of the key"
+  value       = packet_project_ssh_key.this.id
+}
+
+output "name" {
+  description = "The name of SSH key"
+  value       = packet_project_ssh_key.this.name
+}
+
+output "public_key" {
+  description = "The public SSH key"
+  value       = packet_project_ssh_key.this.public_key
+}
+
+output "private_key_filename" {
+  description = "Private key filename"
+  value       = local_file.private_key_pem.filename
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project_ssh_name" {
+  description = "project ssh name"
+  type        = string
+  default     = "project-ssh-name"
+}
+
+variable "project_id" {
+  description = "project id"
+  type        = string
+  default     = ""
+}
